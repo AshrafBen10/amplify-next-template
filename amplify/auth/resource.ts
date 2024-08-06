c7fbaf79b6fda5d73bc28d80d186f3df3d6b597d
Flupinochan <flupino@metalmental.net>
2024-08-07 02:56:54 +0900

    add auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,26 @@
-import { defineAuth } from "@aws-amplify/backend";
+import { defineAuth, secret } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
+// https://docs.amplify.aws/nextjs/build-a-backend/auth/concepts/external-identity-providers/
+// https://docs.amplify.aws/react/deploy-and-host/fullstack-branching/secrets-and-vars/
+
+// ・emailのログインはデフォルトで必須
+// ・ブランチごとのsecretは、Amplifyのコンソール画面から作成可能
+// 　sandbox環境のシークレットは以下のコマンドで設定する
+// 　https://docs.amplify.aws/react/deploy-and-host/fullstack-branching/secrets-and-vars/#local-environment
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      google: {
+        clientId: secret("GOOGLE_CLIENT_ID"),
+        clientSecret: secret("GOOGLE_CLIENT_SECRET"),
+        scopes: ["email"],
+        attributeMapping: {
+          email: "email",
+        },
+      },
+      callbackUrls: ["http://localhost:3000/", "https://www.chatbot.metalmental.net", "https://dev.chatbot.metalmental.net"],
+      logoutUrls: ["http://localhost:3000/", "https://www.chatbot.metalmental.net", "https://www.dev.chatbot.metalmental.net"],
+    },
   },
 });
--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,9 @@ import Button from "@mui/joy/Button";
 import IconButton from "@mui/joy/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { v4 as uuidv4 } from "uuid";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { fetchUserAttributes, fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
+import { Hub } from "aws-amplify/utils";
 
 import { createChat } from "@/app/utils/createChat";
 import { deleteChat } from "@/app/utils/deleteChat";
@@ -37,8 +40,39 @@ export default function App() {
   const [isDeleting, setIsDeleting] = useState<Record<string, boolean>>({});
   const [selectedChat, setSelectedChat] = useState<ChatHistory | null>(null);
 
-  // チャット一覧の取得と監視
+  ////////////
+  /// 認証 ///
+  ////////////
+  const [loginId, setLoginId] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const getAuthenticatedUser = async () => {
+    try {
+      const { username, userId, signInDetails } = await getCurrentUser();
+      const session = await fetchAuthSession({ forceRefresh: true });
+      const attributes = await fetchUserAttributes();
+      if (attributes.email) {
+        setEmail(attributes.email);
+      } else {
+        console.log("Email not found in user attributes");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // Hubで認証関連(サインアップやサインアウト)のイベントリスナーを設定可能
+  Hub.listen("auth", async (data) => {
+    switch (data.payload.event) {
+      case "signedIn": {
+        getAuthenticatedUser();
+      }
+    }
+  });
+
+  //////////////////////////////
+  /// チャット一覧の取得と監視 ///
+  //////////////////////////////
   useEffect(() => {
+    getAuthenticatedUser(); // 認証情報取得
     const sub = client.models.ChatHistory.observeQuery().subscribe({
       next: ({ items }) => {
         const sortedItems = [...items].sort((a, b) => {
@@ -119,46 +153,54 @@ export default function App() {
   /// Rendering ///
   /////////////////
   return (
-    <main>
-      <div className="flex justify-center items-center">
-        <h1 className="text-4xl w-fit p-3 m-3 border-blue-300 border-2">My Chat</h1>
-      </div>
-
-      <div className="flex flex-row">
-        <div className="flex flex-col items-center w-1/6 p-3 m-3 border-blue-300 border-2">
-          <p className="pb-3">left-bar</p>
-          <div className="pb-4">{loading ? <Button loading>New Chat</Button> : <Button onClick={handleNewChat}>New Chat</Button>}</div>
-          {chats.map(({ id, content, createdAt }) => (
-            <Button className="flex flex-row items-center space-x-4 border border-gray-200 rounded-md p-2 mb-2 max-w-full" key={id} variant="outlined" onClick={() => handleDescribeChat(id)}>
-              <p>{formatTimestamp(createdAt)}</p>
-              <IconButton onClick={() => handleDeleteChat(id)} disabled={isDeleting[id]}>
-                <DeleteIcon />
-              </IconButton>
-            </Button>
-          ))}
-        </div>
-
-        <div className="flex flex-col w-4/6 p-3 m-3 border-blue-300 border-2">
-          {selectedChat &&
-            selectedChat.content &&
-            Array.isArray(selectedChat.content) &&
-            selectedChat.content.length > 0 &&
-            typeof selectedChat.content[0] === "string" &&
-            JSON.parse(selectedChat.content[0]).map((message: { role: string; message: string }, index: number) => (
-              <p key={index} className={`break-words px-4 py-2 rounded-lg ${message.role === "user" ? "bg-blue-100" : message.role === "assistant" ? "bg-red-100" : "bg-slate-100"}`}>
-                {message.message}
-              </p>
-            ))}
-          <div className="mt-auto">
-            <div className="pb-3">
-              <Textarea name="Outlined" placeholder="Type in here…" variant="outlined" slotProps={{ textarea: { ref: textareaRef } }} />
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="text-4xl w-fit p-3 m-3 border-blue-300 border-2">My Chat</h1>
+            <p className="pb-4">こんにちは {email} さん</p>
+            <div className="pb-4">
+              <Button onClick={signOut}>Sign out</Button>
             </div>
-            <div className="flex justify-end">{loading ? <Button loading>Create Chat</Button> : <Button onClick={handleCreateChat}>Create Chat</Button>}</div>
           </div>
-        </div>
 
-        <div className="w-1/6 flex justify-center p-3 m-3 border-blue-300 border-2">right-bar</div>
-      </div>
-    </main>
+          <div className="flex flex-row">
+            <div className="flex flex-col items-center w-1/6 p-3 m-3 border-blue-300 border-2">
+              <p className="pb-3">left-bar</p>
+              <div className="pb-4">{loading ? <Button loading>New Chat</Button> : <Button onClick={handleNewChat}>New Chat</Button>}</div>
+              {chats.map(({ id, content, createdAt }) => (
+                <Button className="flex flex-row items-center space-x-4 border border-gray-200 rounded-md p-2 mb-2 max-w-full" key={id} variant="outlined" onClick={() => handleDescribeChat(id)}>
+                  <p>{formatTimestamp(createdAt)}</p>
+                  <IconButton onClick={() => handleDeleteChat(id)} disabled={isDeleting[id]}>
+                    <DeleteIcon />
+                  </IconButton>
+                </Button>
+              ))}
+            </div>
+
+            <div className="flex flex-col w-4/6 p-3 m-3 border-blue-300 border-2">
+              {selectedChat &&
+                selectedChat.content &&
+                Array.isArray(selectedChat.content) &&
+                selectedChat.content.length > 0 &&
+                typeof selectedChat.content[0] === "string" &&
+                JSON.parse(selectedChat.content[0]).map((message: { role: string; message: string }, index: number) => (
+                  <p key={index} className={`break-words px-4 py-2 rounded-lg ${message.role === "user" ? "bg-blue-100" : message.role === "assistant" ? "bg-red-100" : "bg-slate-100"}`}>
+                    {message.message}
+                  </p>
+                ))}
+              <div className="mt-auto">
+                <div className="pb-3">
+                  <Textarea name="Outlined" placeholder="Type in here…" variant="outlined" slotProps={{ textarea: { ref: textareaRef } }} />
+                </div>
+                <div className="flex justify-end">{loading ? <Button loading>Create Chat</Button> : <Button onClick={handleCreateChat}>Create Chat</Button>}</div>
+              </div>
+            </div>
+
+            <div className="w-1/6 flex justify-center p-3 m-3 border-blue-300 border-2">right-bar</div>
+          </div>
+        </main>
+      )}
+    </Authenticator>
   );
 }
