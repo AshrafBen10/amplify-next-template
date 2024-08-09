0261d32a04534f485949ac694d1be5db57f3ed33
Flupinochan <flupino@metalmental.net>
2024-08-10 03:13:15 +0900

    fix google auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,6 +7,8 @@ import { defineAuth, secret } from "@aws-amplify/backend";
 // ・ブランチごとのsecretは、Amplifyのコンソール画面から作成可能
 // 　sandbox環境のシークレットは以下のコマンドで設定する
 // 　https://docs.amplify.aws/react/deploy-and-host/fullstack-branching/secrets-and-vars/#local-environment
+// Google Search Console設定例
+// https://qiita.com/kuro123/items/a6439fec530f91f84e53#google%E8%AA%8D%E8%A8%BC%E3%83%97%E3%83%AD%E3%83%90%E3%82%A4%E3%83%80%E3%83%BC%E3%81%ABamplify%E3%81%AEhostedui%E3%81%AE%E8%A8%AD%E5%AE%9A
 export const auth = defineAuth({
   loginWith: {
     email: true,
@@ -19,8 +21,8 @@ export const auth = defineAuth({
           email: "email",
         },
       },
-      callbackUrls: ["https://be1748a04fef8dfa848d.auth.us-west-2.amazoncognito.com", "http://localhost:3000/", "https://www.dev.chatbot.metalmental.net"],
-      logoutUrls: ["https://be1748a04fef8dfa848d.auth.us-west-2.amazoncognito.com", "http://localhost:3000/", "https://www.dev.chatbot.metalmental.net"],
+      callbackUrls: ["http://localhost:3000/", "https://dev.chatbot.metalmental.net", "https://chatbot.metalmental.net"],
+      logoutUrls: ["http://localhost:3000/", "https://dev.chatbot.metalmental.net", "https://chatbot.metalmental.net"],
       // callbackUrls: ["https://www.chatbot.metalmental.net"],
       // logoutUrls: ["https://www.chatbot.metalmental.net"],
     },
